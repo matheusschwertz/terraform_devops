1162936aec340f47de1bf641bb9d6c591f084db9
matheusschwertz <matheusschwertz@gmail.com>
2024-04-03 21:16:09 -0300

    feat:V1-apresentação

--- main.tf
+++ main.tf
@@ -2,23 +2,32 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = "1.0.1"
+      version = ">=0.1.0"
     }
   }
 }
 
-provider "azuredevops" {
-  # Configuration options
+resource "azuredevops_project" "example" {
+  name               = "Teste Project"
+  work_item_template = "Agile"
+  version_control    = "Git"
+  visibility         = "private"
+  description        = "Managed by Terraform"
 }
 
-resource "azuredevops_project" "example" {
-  name               = "Example Project"
-  visibility         = "private"
-  version_control    = "Git"
-  work_item_template = "Agile"
-  description        = "Managed by Terraform"
-  features = {
-    "testplans" = "disabled"
-    "artifacts" = "disabled"
+data "azuredevops_group" "example-project-readers" {
+  project_id = azuredevops_project.example.id
+  name       = "Readers"
+}
+
+resource "azuredevops_area_permissions" "example-root-permissions" {
+  project_id = azuredevops_project.example.id
+  principal  = data.azuredevops_group.example-project-readers.id
+  path       = "/readers"
+  permissions = {
+    CREATE_CHILDREN = "Deny"
+    GENERIC_READ    = "Allow"
+    DELETE          = "Deny"
+    WORK_ITEM_READ  = "Allow"
   }
 }
